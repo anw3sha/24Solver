7d91856ac4adf9fa6586d08a6e95734982e3f6f6
Anwesha Pathak <anweshap@umich.edu>
2024-01-28 16:50:51 -0500

    new file:   .vscode/settings.json 	modified:   24solver.cpp 	new file:   solved

--- 24solver.cpp
+++ 24solver.cpp
@@ -3,16 +3,87 @@
 #include <cmath>
 #include <algorithm>
 
-std::string solver(int w, int x, int y, int z){
-    int nums[] = {w, x, y, z};
-    std::vector<std::string> solution = {};
+using namespace std;
+
+vector<vector<int> > swap1(int arr[]){
+    vector<vector<int> > swaps;
+    for (size_t i = 0; i < 24; ++i){ // 4! = 24
+        next_permutation(arr, arr+4);
+        swaps.push_back(vector<int>(arr, arr+4));
+    }
+    return swaps; 
 }
 
-std::vector<std::vector<int>> swap(int arr[]){
-    std::vector<std::vector<int>> swaps;
-    for (size_t i = 0; i < 25; ++i){
-        swaps[i] = std::next_permutation(arr, arr+3);
+vector<vector<char> > swap2(char ops[], int n, int r) {
+    vector<vector<char> > combinations;
+    vector<char> combination(r);
+    int index[r];
+
+    for (int i = 0; i < r; i++)
+        index[i] = 0;
+
+    while (true) {
+        for (int i = 0; i < r; i++)
+            combination[i] = ops[index[i]];
+        combinations.push_back(combination);
+
+        int next = r - 1;
+        while (next >= 0 && (index[next] + 1 >= n))
+            next--;
+
+        if (next < 0)
+            return combinations;
+
+        index[next]++;
+
+        for (int i = next + 1; i < r; i++)
+            index[i] = 0;
     }
+}
+
+int calculateResult(int a, int b, char operation) {
+    if (operation == '+') {
+        return a + b;
+    } else if (operation == '-') {
+        return a - b;
+    } else if (operation == '*') {
+        return a * b;
+    } else if (operation == '/') {
+        if (b != 0) {
+            return a / b;
+        } else {
+            return INT_MIN; // undefined
+        }
+    } 
+    return INT_MIN; 
+}
+
+vector<string> solver(int v, int w, int x, int y, int z){
+    int nums[] = {v, w, x, y};
+    char ops[] = {'+', '-', '*', '/'};
+    vector<string> solution;
+    vector<vector<int> > numbers = swap1(nums);
+    vector<vector<char> > operations = swap2(ops, 4, 3); // 4 operations, 3 slots
+
+    for (size_t i = 0; i < numbers.size(); ++i){
+        for (size_t j = 0; j < operations.size(); ++j){
+            int result = calculateResult(numbers[i][0], numbers[i][1], operations[j][0]);
+            result = calculateResult(result, numbers[i][2], operations[j][1]);
+            result = calculateResult(result, numbers[i][3], operations[j][2]);
+            if (result == INT_MIN) {continue;}
+            if (result == z){
+                string s = to_string(numbers[i][0]) + string(1, operations[j][0]) + 
+                to_string(numbers[i][1]) + string(1, operations[j][1]) + 
+                to_string(numbers[i][2]) + string(1, operations[j][2]) + 
+                to_string(numbers[i][3]);
+
+                solution.push_back(s);
+            }
+        }
+    }
+
+    return solution;
+
 }
 
 int main(){
@@ -20,10 +91,16 @@ int main(){
     int b;
     int c;
     int d;
+    int target;
 
-    std::cout << "Enter four integers separated by a space: ";
-    std::cin >> a >> b >> c >> d;
+    cout << "Enter four integers and then your target separated by a space: ";
+    cin >> a >> b >> c >> d >> target;
 
-    std::string solution = solver(a,b,c,d);
-    std::cout << solution; 
+    vector<string> solutions = solver(a,b,c,d,target);
+    string solution = "";
+    for (size_t i = 0; i < solutions.size(); ++i) {
+    const string& s = solutions[i];
+    solution += s + "\n";
+    }
+    cout << solution; 
 }
